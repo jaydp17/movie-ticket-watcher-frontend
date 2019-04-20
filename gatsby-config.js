536e02985b8d6bf47322d2e677edb5fc7cb657b7
Jaydeep Solanki <jaydp17@gmail.com>
2019-04-20 19:13:19 +0530

    add graphql env vars to build for staging/production

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         short_name: `BMS Notifier`,
         start_url: `/`,
         background_color: `#663399`,
-        
+
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
@@ -34,11 +34,11 @@ module.exports = {
         typeName: 'API',
         fieldName: 'api',
         // Url to query from
-        url: 'http://localhost:62222/graphql',
+        url: process.env.SERVER_GRAPHQL_ENDPOINT || 'http://localhost:62222/graphql',
         // HTTP headers
         headers: {
           // Learn about environment variables: https://gatsby.dev/env-vars
-          'x-api-key': 'abc',
+          'x-api-key': process.env.SERVER_API_KEY || 'abc',
         },
       },
     },
