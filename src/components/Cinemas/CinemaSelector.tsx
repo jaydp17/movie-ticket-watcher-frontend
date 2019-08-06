8f5cce3290e15440211d3f5b2f1c47955ee4b1fa
Jaydeep Solanki <jaydp17@gmail.com>
2019-08-06 17:17:49 +0530

    add cinema selector as well

---
+++ src/components/Cinemas/CinemaSelector.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import useCinemas from './useCinemas';
+
+interface Props {
+  cityID: string;
+  selectCinema: (cinemaID: string) => void;
+}
+function CinemaSelector({ cityID, selectCinema }: Props) {
+  const [cinemas, isLoading] = useCinemas(cityID);
+  const [cinemaID, setCinema] = useState();
+
+  const onCinemaChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedID = e.target.value;
+    if (!selectedID) return;
+    setCinema(selectedID);
+    selectCinema(selectedID);
+  };
+
+  return (
+    <fieldset>
+      <legend>Cinemas</legend>
+      {isLoading && <p>loading...</p>}
+      {!isLoading && (
+        <select name="cinemaID" value={cinemaID} onChange={onCinemaChange}>
+          {cinemas.map(c => (
+            <option key={c.id} value={c.id}>
+              {c.name}
+            </option>
+          ))}
+        </select>
+      )}
+    </fieldset>
+  );
+}
+
+export default CinemaSelector;
--- src/components/SubscriptionForm/SubscriptionForm.tsx
+++ src/components/SubscriptionForm/SubscriptionForm.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
+import CinemaSelector from '../Cinemas';
 import CitySelector from '../Cities';
 import MovieSelector from '../Movies';
 
 function SubscriptionForm() {
   const [cityID, setCityID] = useState('BANG');
   const [movieID, setMovieID] = useState();
+  const [cinemaID, setCinemaID] = useState();
   return (
     <form>
       <CitySelector selectCity={setCityID} />
       <MovieSelector cityID={cityID} selectMovie={setMovieID} />
+      <CinemaSelector cityID={cityID} selectCinema={setCinemaID} />
     </form>
   );
 }
