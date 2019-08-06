bbf2fe7a40f973645d7c109f9e79b1cf9c1ef783
Jaydeep Solanki <jaydp17@gmail.com>
2019-08-06 17:34:08 +0530

    add date picker

--- src/components/SubscriptionForm/SubscriptionForm.tsx
+++ src/components/SubscriptionForm/SubscriptionForm.tsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react';
 import CinemaSelector from '../Cinemas';
 import CitySelector from '../Cities';
 import MovieSelector from '../Movies';
+import DatePicker from '../DatePicker';
+import { toYYYY_MM_DD } from '../../helpers/date-helpers';
 
 function SubscriptionForm() {
   const [cityID, setCityID] = useState('BANG');
   const [movieID, setMovieID] = useState();
   const [cinemaID, setCinemaID] = useState();
+  const [date, setDate] = useState(() => toYYYY_MM_DD(new Date()));
   return (
     <form>
       <CitySelector selectCity={setCityID} />
       <MovieSelector cityID={cityID} selectMovie={setMovieID} />
       <CinemaSelector cityID={cityID} selectCinema={setCinemaID} />
+      <DatePicker selectDate={setDate} />
     </form>
   );
 }
