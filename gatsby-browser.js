9b98224774639b8b0a3f230c0226af4b775d9180
Jaydeep Solanki <jaydp17@gmail.com>
2019-08-08 18:37:09 +0530

    add a service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,29 @@
  */
 
 // You can delete this file if you're not using it
+// import('babel-loader?name=sworker.js!./src/service-worker.ts');
+
+// export const registerServiceWorker = () => true;
+
+export const onClientEntry = () => {
+  if ('serviceWorker' in navigator) {
+    if (!('PushManager' in window)) {
+      alert('Your browser does not support Web Push!');
+    } else {
+      window.addEventListener('load', async () => {
+        const registration = await navigator.serviceWorker.register('/sw.js');
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      });
+    }
+  }
+};
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. Reload to display the latest version?`,
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
---
+++ static/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('push', e => {
+  console.log('push event', e);
+  const data = e.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body || 'hello',
+    icon: data.icon,
+  });
+});
