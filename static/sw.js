d4d57f2559a4f1d3725aae087ad4fe542a704166
Jaydeep Solanki <jaydp17@gmail.com>
2019-08-17 01:09:30 +0530

    show notification

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,11 @@
 self.addEventListener('push', e => {
   console.log('push event', e);
   const data = e.data.json();
+  console.log('push data', data);
   self.registration.showNotification(data.title, {
     body: data.body || 'hello',
     icon: data.icon,
+    vibrate: data.vibrate,
+    actions: data.actions,
   });
 });
