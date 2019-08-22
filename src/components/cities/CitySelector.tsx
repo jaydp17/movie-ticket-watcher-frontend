8f889b1209162583bd09f8f7a46879d0a06e4b33
Jaydeep Solanki <jaydp17@gmail.com>
2019-08-22 20:26:17 +0530

    use Material-UI & downshift for CitySelector

--- src/components/cities/CitySelector.tsx
+++ src/components/cities/CitySelector.tsx
@@ -1,5 +1,23 @@
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import DownshiftComponent, { DownshiftInterface } from 'downshift';
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import { ICity } from './types';
 import useCities from './useCities';
+
+function getDownShift<T>() {
+  return DownshiftComponent as DownshiftInterface<T>;
+}
+
+const Downshift = getDownShift<ICity>();
+
+const ScrollablePaper = styled(Paper)`
+  max-height: 200px;
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
 
 interface Props {
   selectCity: (cityID: string) => void;
@@ -10,24 +28,107 @@ function CitySelector({ selectCity }: Props) {
 
   if (isLoading) return <p>loading...</p>;
 
-  const onCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedID = e.target.value;
+  const onCityChange = (selectedID: string | null) => {
     if (!selectedID) return;
     setCityID(selectedID);
     selectCity(selectedID);
   };
 
   return (
-    <fieldset>
-      <legend>Cities</legend>
-      <select name="cityID" value={cityID} onChange={onCityChange}>
-        {cities.map(c => (
-          <option key={c.id} value={c.id}>
-            {c.name}
-          </option>
-        ))}
-      </select>
-    </fieldset>
+    <Downshift
+      onChange={selection => onCityChange(selection && selection.id)}
+      initialSelectedItem={{ id: 'BANG', name: 'Bengaluru', isTopCity: true }}
+      itemToString={item => (item == null ? '' : item.name)}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getLabelProps,
+        getMenuProps,
+        isOpen,
+        inputValue,
+        highlightedIndex,
+        openMenu,
+        selectedItem,
+      }) => {
+        const { onBlur, onChange, onFocus, ...inputProps } = getInputProps({ onFocus: openMenu });
+        return (
+          <div>
+            <div>
+              {renderInput({
+                fullWidth: true,
+                label: 'City',
+                InputLabelProps: getLabelProps(),
+                InputProps: { onBlur, onChange, onFocus },
+                inputProps,
+              })}
+            </div>
+            <ScrollablePaper {...getMenuProps()}>
+              {isOpen
+                ? cities
+                    .filter(city =>
+                      inputValue
+                        ? city.name.toLowerCase().includes(inputValue.toLowerCase())
+                        : city.isTopCity,
+                    )
+                    .map((city, index) =>
+                      renderCityItem({
+                        city,
+                        highlightedIndex,
+                        index,
+                        selectedItem,
+                        itemProps: getItemProps({ item: city }),
+                      }),
+                    )
+                : null}
+            </ScrollablePaper>
+          </div>
+        );
+      }}
+    </Downshift>
+  );
+}
+
+interface CityItemProps {
+  highlightedIndex: null | number;
+  index: number;
+  itemProps: object;
+  selectedItem: ICity | null;
+  city: ICity;
+}
+function renderCityItem(cityItemProps: CityItemProps) {
+  const { city, index, itemProps, highlightedIndex, selectedItem } = cityItemProps;
+  const isHighlighted = highlightedIndex === index;
+  const isSelected = selectedItem && selectedItem.id === city.id;
+
+  return (
+    <MenuItem
+      {...itemProps}
+      key={city.id}
+      selected={isHighlighted}
+      component="div"
+      style={{ fontWeight: isSelected ? 500 : 400 }}
+    >
+      {city.name}
+    </MenuItem>
+  );
+}
+
+function renderInput(inputProps: any) {
+  const { InputProps, classes, ref, InputLabelProps, ...other } = inputProps;
+
+  return (
+    <TextField
+      InputProps={{
+        inputRef: ref,
+        ...InputProps,
+      }}
+      InputLabelProps={{
+        ...InputLabelProps,
+        shrink: true,
+      }}
+      {...other}
+    />
   );
 }
 
